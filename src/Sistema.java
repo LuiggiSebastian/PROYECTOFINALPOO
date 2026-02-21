85645eb7e623bf42bd761a0633d35965d288cb59
luigg <luigg@AcerNitro5>
2026-02-21 11:40:20 -0500

    Prueba 2 con de Github

--- src/Sistema.java
+++ src/Sistema.java
@@ -2,4 +2,18 @@
 public class Sistema {
     private String nombre;
     //prueba commit
+
+    public Sistema() {
+        nombre="";
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
 }
