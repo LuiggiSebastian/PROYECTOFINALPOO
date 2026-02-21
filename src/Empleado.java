ac8920c5baa705ef1d947c833a8ea66698a402cf
luigg <luigg@AcerNitro5>
2026-02-21 10:47:29 -0500

    Prueba 1 de Github

---
+++ src/Empleado.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Empleado {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Hola son 1 o 2?:");
+        int n;
+        n=sc.nextInt();
+        if (n==1) {
+            System.out.println("1"); 
+        }else{
+            System.out.println("2");
+        }
+    }
+    
+}
