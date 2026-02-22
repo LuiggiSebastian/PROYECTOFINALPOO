1c6c56a474710ca09365cb2bbee0ddc5c0fa1746
ALVARO <ALVARO@192.168.56.1>
2026-02-22 00:40:38 -0500

    prueba 4

--- src/modelo/abstractas/Persona.java
+++ src/modelo/abstractas/Persona.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo.abstractas;
 
-/**
- *
- * @author luigg
- */
-public class Persona {
-    
+public abstract class Persona {
+    protected int dni;
+    protected String nombres;
+    protected String apellidos;
+
+    public Persona() {
+    }
+
+    public Persona(int dni, String nombres, String apellidos) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombreCompleto() {
+        return nombres + " " + apellidos;
+    }
+
+    public void mostrarDatosBasicos() {
+        System.out.println("DNI: " + dni);
+        System.out.println("Nombres: " + nombres);
+        System.out.println("Apellidos: " + apellidos);
+    }
 }
+    
+
